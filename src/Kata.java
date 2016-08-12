baf1f3d76d1aac96cc1d65f745e5a8bec6180162
jeffreydorney <jjdorney1@gmail.com>
2016-08-12 14:13:18 -0400

    not string and missing char katas added; successful print

--- src/Kata.java
+++ src/Kata.java
@@ -3,6 +3,8 @@
  */
 public class Kata {
 
+
+    // method to add a space between every letter of the word given
     public String explode(String inputWord) {
 
         // sets length to the input word length
@@ -50,4 +52,42 @@ public class Kata {
     //public char letters(String inputWord, int inputWordLength) {
 
     }
+
+    // method to add "not" to the front of the string, unless it is already there
+    public String notString(String inputString) {
+
+        // checks to see if it begins with "not" if so it'll return the string
+        if(inputString.startsWith("not")) {
+            return inputString;
+
+        } else {
+
+            // concatenates "not" to the front of the string and returns it
+            inputString = "not " + inputString;
+            return inputString;
+        }
+    }
+
+    // method to remove a given character from a string at a defined location in the string
+    public String missingChar(String inputString, int n) {
+
+        // checks to see if the length is bigger than the number and runs if it is
+        if(inputString.length() >= n) {
+
+            // sets the front string to the first part of the word up to the given number
+            String front = inputString.substring(0, n);
+
+            // sets the back string to the remaining part the word until the end
+            String back = inputString.substring(n + 1);
+
+            // concatenates the new front and back without the numbered letter
+            return front + back;
+        } else {
+
+            // returns the string with no changes if the number is longer than the word
+            return inputString;
+        }
+    }
+
+    
 }
--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,30 @@ public class Main {
 
         Kata kata = new Kata();
 
-        System.out.println( "explode(\"example\") -> \"" + kata.explode("example") + "\"" );
-        System.out.println( "explode(\"cat\") -> \"" + kata.explode("cat") + "\"" );
-        System.out.println( "explode(\"\") -> \"" + kata.explode("") + "\"" );
-        System.out.println( "explode(\"c\") -> \"" + kata.explode("c") + "\"" );
-        
+        // output for the explode kata including the string value that was given
+        System.out.println("Explode Kata:\n**********************");
+        System.out.println("explode(\"example\") --> \"" + kata.explode("example") + "\"" );
+        System.out.println("explode(\"cat\") --> \"" + kata.explode("cat") + "\"" );
+        System.out.println("explode(\"\") --> \"" + kata.explode("") + "\"" );
+        System.out.println("explode(\"c\") --> \"" + kata.explode("c") + "\"" );
+
+        // line break
+        System.out.println("\n");
+
+        // output for the not string kata including the string value that was given
+        System.out.println("Not String Kata:\n**********************");
+        System.out.println("notString(\"candy\") --> \"" + kata.notString("candy") + "\"");
+        System.out.println("notString(\"x\") --> \"" + kata.notString("x") + "\"");
+        System.out.println("notString(\"not bad\") --> \"" + kata.notString("not bad") + "\"");
+
+        // line break
+        System.out.println("\n");
+
+        // output for the missing char kata including the string value that was given
+        System.out.println("Missing Char Kata:\n**********************");
+        System.out.println("missingChar(\"kitten\", 1) --> \"" + kata.missingChar("kitten", 1)+ "\"");
+        System.out.println("missingChar(\"kitten\", 0) --> \"" + kata.missingChar("kitten", 0)+ "\"");
+        System.out.println("missingChar(\"kitten\", 4) --> \"" + kata.missingChar("kitten", 4)+ "\"");
+
     }
 }
